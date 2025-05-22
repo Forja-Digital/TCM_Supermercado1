b51bc83bcf8d718f228ff3d98868eb7b05cb3ebe
RuanPSilva <rpfernandes71@gmail.com>
2025-05-21 21:53:28 -0300

    Atualizando index e css da Login

--- obj/Debug/net8.0/TCM_Supermercado1.AssemblyInfo.cs
+++ obj/Debug/net8.0/TCM_Supermercado1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TCM_Supermercado1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edb4c6c08d04aedc72b2a730f3db04c5556241eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TCM_Supermercado1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TCM_Supermercado1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
