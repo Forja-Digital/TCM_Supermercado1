ebce2d82b75ad60805600fc98967966376827b3a
Jackhabbit10 <mauricioikier@hotmail.com>
2025-06-12 23:12:08 -0300

    Finalizando o método de login

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -8,9 +8,32 @@ namespace TCM_Supermercado1.Controllers
 
         private readonly LoginRepositorio _loginRepositorio;
 
+        public LoginController(LoginRepositorio loginRepositorio)
+        {
+            _loginRepositorio = loginRepositorio;
+        }
         public IActionResult Login()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            var funcionario = _loginRepositorio.ObterFuncionario(email);
+
+            if (funcionario != null && funcionario.senha_funcionario == senha)
+            {
+                // Autenticação bem-sucedida
+                // Redireciona o usuário para a action "Index" do Controller "Cliente".
+                return RedirectToAction("RecuperarSenha", "Home");
+            }
+
+            ModelState.AddModelError("", "Email ou senha inválidos.");
+
+            //retorna view Login 
+            return View();
+        }
+
     }
 }
